60c2c75cf5197aa6a67fc10b84c60ffcf5dd15d9
Lutfi Damaera <damaera@live.com>
2022-09-22 21:09:11 +0700

    add item

--- src/components/TodoList/item.jsx
+++ src/components/TodoList/item.jsx
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types'
+import './item.css'
 
 export const TodoListItem = ({ id, name, onClickDelete }) => {
   return (
-    <div data-testid={`todo-item-${id}`}>
-      <div className="name">{name.trim()}</div>
-      <button onClick={() => onClickDelete(id)}>delete</button>
+    <div data-testid={`todo-item-${id}`} className="item">
+      <div className="item__name">{name.trim()}</div>
+      <button className="item__delete" onClick={() => onClickDelete(id)}>
+        Delete
+      </button>
     </div>
   )
 }
